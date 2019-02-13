6e314614c4b917915161bd66c222e67061641fcb
K R <theoden8@gmail.com>
2019-02-13 04:53:07 +0000

    uncommented graph generation

--- plot_homomorphism.py
+++ plot_homomorphism.py
@@ -8,11 +8,11 @@ from homomorphism_solver import *
 if __name__ == "__main__":
     plt.switch_backend('agg')
     # G, gname = make_random_graph()
-    # G = nx.binomial_graph(7, .4)
-    # nx.write_gpickle(G, 'graph.pkl')
+    G = nx.binomial_graph(7, .4)
+    nx.write_gpickle(G, 'graph.pkl')
     G = nx.read_gpickle('graph.pkl')
-    # H, phi = make_random_homomorphism(G)
-    # nx.write_gpickle(H, 'graph_morph.pkl')
+    H, phi = make_random_homomorphism(G)
+    nx.write_gpickle(H, 'graph_morph.pkl')
     H = nx.read_gpickle('graph_morph.pkl')
     for soln in is_homomorphism(G, H):
         morph = soln
