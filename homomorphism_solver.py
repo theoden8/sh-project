8448a7549b6bf12d128a62d255b6a5b8bfd265bc
K R <theoden8@gmail.com>
2019-02-16 15:18:06 +0000

    improved readability and heuristics of homomorphism solver

--- homomorphism_solver.py
+++ homomorphism_solver.py
@@ -15,37 +15,22 @@ class Solver:
         self.g = g
         self.h = h
         self.possibles = [
-            sorted(list(self.h.nodes()), key=lambda x: abs(self.g.degree()[nd] - self.h.degree()[x]))
-            # shifted(list(h.nodes()), nd)
+            # sorted(list(self.h.nodes()), key=lambda x: abs(self.g.degree()[nd] - self.h.degree()[x]))
+            shifted(list(h.nodes()), nd)
             # sorted(list(self.h.nodes()))
                 for nd in range(len(self.g.nodes()))
         ]
         # self.srcs = [Solver.UNDEFINED for nd in self.g.nodes()]
         # self.srcs = shuffled(list(range(len(self))))
-        # self.srcs = list(range(len(self)))
-        self.srcs = sorted(list(self.g.nodes()), key=lambda nd: self.g.degree()[nd], reverse=True)
+        self.srcs = list(range(len(self)))
+        # self.srcs = sorted(list(self.g.nodes()), key=lambda nd: self.g.degree()[nd], reverse=True)
         self.soln_inds = [Solver.UNDEFINED for nd in self.g.nodes()]
         self.soln = [Solver.UNDEFINED for nd in self.g.nodes()]
         self.i = 0
         self.action = Solver.FORWARD
+        self.no_solns = 0
 
-    def reset_soln_cell(self):
-        i = 0 if self.i == -1 else self.i
-        ind = self.srcs[i]
-        # self.srcs[i] = Solver.UNDEFINED
-        self.soln_inds[ind] = Solver.UNDEFINED
-        self.soln[ind] = Solver.UNDEFINED
-
-    def set_soln_ind(self, sln_ind):
-        i = 0 if self.i == -1 else self.i
-        ind = self.srcs[i]
-        self.soln_inds[ind] = sln_ind
-        self.soln[ind] = self.possibles[ind][self.soln_inds[ind]]
-
-    def set_next_soln(self):
-        self.set_soln_ind(self.soln_ind() + 1)
-
-    def is_valid_soln(self):
+    def is_valid_solution(self):
         dbg = (self.soln == [0, 0, 0, 1, 3, 5, 6])
         assert self.i == len(self)
         gnodes = list(self.g.nodes())
@@ -57,7 +42,21 @@ class Solver:
                 return False
         return True
 
-    def is_final_option(self, val=None):
+    def choose_best_node(self):
+        option, rating = -1, -1
+        available = [x for x in range(len(self)) if x not in self.srcs[:self.i]]
+        for ind in available:
+            new_rating = 0
+            new_rating += 100 * len([nd for nd in list(self.g.neighbors(ind)) if nd in self.srcs[:self.i]])
+            new_rating += 50 * len([nd for nd in list(self.g.neighbors(ind)) if nd not in self.srcs[:self.i]])
+            if new_rating > rating:
+                option = ind
+                rating = new_rating
+        if option != -1:
+            return option
+        return self.srcs[self.i]
+
+    def is_last_option(self, val=None):
         i = 0 if self.i < 0 else self.i
         ind = self.srcs[i]
         if val is None:
@@ -71,77 +70,103 @@ class Solver:
             val = self.soln_ind()
         return val in range(len(self.possibles[ind]))
 
+    def choose_target_order(self):
+        i = 0 if self.i < 0 else self.i
+        ind = self.srcs[i]
+        def rating_func(h_ind):
+            map_image = [self.soln[idx] for idx in self.srcs[:self.i]]
+            rating = 0
+            rating += 100 * len([nd for nd in list(self.h.neighbors(h_ind)) if nd in map_image])
+            rating += 10 * len([nd for nd in list(self.h.neighbors(h_ind)) if nd not in map_image])
+            return rating
+        self.possibles[ind] = sorted(self.possibles[ind], key=rating_func, reverse=True)
+
+    def find_possible_map(self):
+        i = 0 if self.i < 0 else self.i
+        ind = self.srcs[i]
+        mapto = self.soln_ind() + 1
+        # print(self.soln)
+        while self.is_valid_option(mapto):
+            approved = True
+            if i > 0:
+                gu = ind
+                hu = self.possibles[ind][mapto]
+                for j in range(i):
+                    gv = self.srcs[j]
+                    hv = self.soln[gv]
+                    if (gu, gv) in self.g.edges() and (hu, hv) not in self.h.edges():
+                        approved = False
+                        break
+            if approved:
+                break
+            mapto += 1
+        return mapto
+
+    def forward_node(self, mapto):
+        i = 0 if self.i == -1 else self.i
+        ind = self.srcs[i]
+        self.action = Solver.FORWARD
+        self.soln_inds[ind] = mapto
+        self.soln[ind] = self.possibles[ind][self.soln_inds[ind]]
+        self.i += 1
+
+    def set_rollback(self):
+        i = 0 if self.i == -1 else self.i
+        ind = self.srcs[i]
+        self.action = Solver.BACKTRACK
+        # self.srcs[i] = Solver.UNDEFINED
+        self.soln_inds[ind] = Solver.UNDEFINED
+        self.soln[ind] = Solver.UNDEFINED
+        self.i -= 1
+
     def soln_ind(self):
         i = 0 if self.i < 0 else self.i
         ind = self.srcs[i]
         return self.soln_inds[ind]
 
+    def find_solutions(self, function=None):
+        if function is None:
+            def func(soln):
+                self.no_solns += 1
+            function = func
+        while True:
+            while self.i in range(len(self)):
+                if self.i < 3:
+                    print(self)
+                if self.action == Solver.FORWARD:
+                    self.srcs[self.i] = self.choose_best_node()
+                    if self.is_last_option():
+                        self.set_rollback()
+                assert self.i >= -1
+                if self.action == Solver.BACKTRACK and self.soln_ind() != Solver.UNDEFINED:
+                    if self.i < len(self) / 2:
+                        self.choose_target_order()
+                    pass
+                mapto = self.find_possible_map()
+                if self.is_valid_option(mapto):
+                    self.forward_node(mapto)
+                else:
+                    self.set_rollback()
+            if self.i < 0:
+                break
+            if self.is_valid_solution():
+                function(self.soln)
+            # deal with result
+            self.i -= 1
+            self.action = Solver.BACKTRACK
+        if self.i != -1:
+            function(self.soln)
+
     def __str__(self):
         s = 'backtrack' if self.action else 'forward'
-        s += ' ' + str(self.i) + ' soln:' + str(self.soln_inds)
+        s += ' ' + str(self.i) + ' soln:' + str([self.soln_inds[x] for x in self.srcs])
         return s
 
     def __len__(self):
         return len(self.g.nodes())
 
 
-def is_homomorphism(g, h):
+def find_homomorphisms(g, h):
     s = Solver(g, h)
-    print(s.possibles)
-    while True:
-        while s.i in range(len(s)):
-            if s.i < len(s) / 3:
-                print(s)
-            if s.action == Solver.FORWARD:
-                # can choose next option here
-                # options = [j for j in range(len(s.soln)) if j not in s.srcs[:s.i]]
-                # options = sorted(options, key=lambda x: s.g.degree()[x])
-                # s.srcs[s.i] = options[-1]
-
-                # we just used our last option
-                if s.is_final_option():
-                    s.action = Solver.BACKTRACK
-                    s.reset_soln_cell()
-                    s.i -= 1
-            assert s.i >= -1
-            if s.action == Solver.BACKTRACK and s.soln_ind() != Solver.UNDEFINED:
-                # shuffle possibles?
-                pass
-            new_soln_ind = s.soln_ind() + 1
-            while not s.is_final_option(new_soln_ind) and s.is_valid_option(new_soln_ind):
-                # check constraint directly here
-                approved = True
-                ii = 0 if s.i < 0 else s.i
-                if ii > 0:
-                    # take all previous indices
-                    for j in range(ii):
-                        gu, gv = s.srcs[ii], s.srcs[j]
-                        hu, hv = s.possibles[gu][new_soln_ind], s.soln[gv]
-                        edge_g, edge_h = (gu, gv), (hu, hv)
-                        # print('test', edge_g, edge_h)
-                        if edge_g in s.g.edges() and edge_h not in s.h.edges():
-                            approved = False
-                            # print('not ok')
-                            break
-                if approved:
-                    break
-                new_soln_ind += 1
-            if not s.is_final_option():
-                s.action = Solver.FORWARD
-                # s.srcs[ii] = s.i
-                s.set_soln_ind(new_soln_ind)
-                s.i += 1
-            else:
-                s.action = Solver.BACKTRACK
-                s.reset_soln_cell()
-                s.i -= 1
-            # print(s)
-        if s.i < 0:
-            break
-        if s.is_valid_soln():
-            yield s.soln
-        # deal with result
-        s.i -= 1
-        s.action = Solver.BACKTRACK
-    if s.i != -1:
-        yield s.soln
+    s.find_solutions()
+    return s.no_solns
--- plot_homomorphism.py
+++ plot_homomorphism.py
@@ -8,14 +8,17 @@ from homomorphism_solver import *
 if __name__ == "__main__":
     plt.switch_backend('agg')
     # G, gname = make_random_graph()
-    G = nx.binomial_graph(18, .4)
+    G = nx.binomial_graph(24, .4)
+    print('nodes', len(list(G.nodes())), 'edges', len(list(G.edges())))
     nx.write_gpickle(G, 'graph.pkl')
     G = nx.read_gpickle('graph.pkl')
     H, phi = make_random_homomorphism(G)
     nx.write_gpickle(H, 'graph_morph.pkl')
     H = nx.read_gpickle('graph_morph.pkl')
-    for soln in is_homomorphism(G, H):
-        morph = soln
-        print('homomorphism', soln)
-        plot_homomorphism(G, H, morph, filename='homomorphism.png')
-        break
+    no_solns = 0
+    solutions = find_homomorphisms(G, H)
+    # if len(solutions) > 0:
+        # soln = solutions[0]
+        # print('homomorphism', soln)
+    plot_homomorphism(G, H, 'title', filename='homomorphism.png')
+    print(solutions)
