254a1ce89806be6a47ac74a85a0616a6aa8be7bb
K R <theoden8@gmail.com>
2019-03-13 04:35:16 +0000

    removed previous code from homomorphism visualizer

--- plot_homomorphism.py
+++ plot_homomorphism.py
@@ -9,13 +9,6 @@ from homomorphism_solver import *
 
 if __name__ == "__main__":
     plt.switch_backend('agg')
-    # G = make_random_graph(45)
-    # print('nodes', len(list(G.nodes())), 'edges', len(list(G.edges())))
-    # nx.write_gpickle(G, 'graph.pkl')
-    # G = nx.read_gpickle('graph.pkl')
-    # H, phi = make_random_homomorphism(G)
-    # nx.write_gpickle(H, 'graph_morph.pkl')
-    # H = nx.read_gpickle('graph_morph.pkl')
     gfile, hfile = sys.argv[1], sys.argv[2]
     G, H = None, None
     with open(gfile, 'r') as f:
